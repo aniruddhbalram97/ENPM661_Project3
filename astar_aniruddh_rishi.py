8ee1f79204ff203df6fe995259f50d39528e6584
Aniruddh Balram <abalram1@umd.edu>
2023-03-19 21:21:14 -0400

    Added function to check for valid inputs

--- astar_aniruddh_rishi.py
+++ astar_aniruddh_rishi.py
@@ -27,3 +27,18 @@ def enter_coordinates(obstacle_map):
     i = (x_i, obstacle_map.shape[0] - y_i - 1, orientation_s)
     g = (x_g, obstacle_map.shape[0] - y_g - 1, orientation_g)
     return i, g
+
+# check for valid coordinates
+def check_valid_entry(start, goal, obstacle_map):
+    if(obstacle_map[start[1] + 5][start[0] + 5][0]==255 or obstacle_map[goal[1]+ 5][goal[0] + 5][0]==255 or 
+       obstacle_map[start[1] - 5][start[0] - 5][0]==255 or obstacle_map[goal[1]- 5][goal[0] - 5][0]==255 or 
+      (start[0] - 5) < 0 or (start[0] + 5) > 599 or (start[1] - 5) < 0 or (start[1] + 5) > 249 or sum(obstacle_map[start[1] + 5][start[0] + 5])==765
+       or sum(obstacle_map[goal[1]][goal[0]])==765 or sum(obstacle_map[start[1] - 5][start[0] - 5])==765
+       or sum(obstacle_map[goal[1] - 5][goal[0] - 5])==765
+      ):
+        print("The start point or end point is invalid. Either it is out of bounds or within obstacle space. Try Again!\n")
+        return False
+    else:
+        print("Great! The start and goal points are valid")
+        return True
+
