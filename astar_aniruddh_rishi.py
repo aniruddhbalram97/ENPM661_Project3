45af2cf36309b9aa2b5583869b7bf0f2b8c3ab5d
Aniruddh Balram <abalram1@umd.edu>
2023-03-19 21:22:14 -0400

    Added function to generate the map

--- astar_aniruddh_rishi.py
+++ astar_aniruddh_rishi.py
@@ -42,3 +42,86 @@ def check_valid_entry(start, goal, obstacle_map):
         print("Great! The start and goal points are valid")
         return True
 
+# Function to generate map using half plane equations
+def generate_map(canvas):
+    # Points for hexagon
+    a = 75
+    h = int((math.sqrt(3)/2) * a)
+    # Hexagon with clearance
+    x1, y1 = 300 - h - 5, int(125 - a/2 - 2.5)
+    x2 ,y2 = 300, int(125 - a - 5)
+    m1 = (y2 - y1)/(x2 - x1) # slope of left top line
+    x3, y3 = 300 - h - 5, int(125 + a/2 + 2.5)
+    x4 ,y4 = 300, int(125 + a + 5)
+    m2 = (y4 - y3)/(x4 - x3)
+    x5, y5 = 300 + h + 5, int(125 + a/2 + 2.5)
+    m3 = (y5 - y4)/(x5 - x4)
+    x6, y6 = 300 + h + 5, int(125 - a/2 - 2.5)
+    m4 = (y6 - y2)/(x6 - x2)
+    
+    # Hexagon without clearance
+    x1_, y1_ = 300 - h, int(125 - a/2)
+    x2_ ,y2_ = 300, int(125 - a)
+    m1_ = (y2_ - y1_)/(x2_ - x1_) # slope of left top line
+    x3_, y3_ = 300 - h, int(125 + a/2)
+    x4_ ,y4_ = 300, int(125 + a)
+    m2_ = (y4_ - y3_)/(x4_ - x3_)
+    x5_, y5_ = 300 + h, int(125 + a/2)
+    m3_ = (y5_ - y4_)/(x5_ - x4_)
+    x6_, y6_ = 300 + h, int(125 - a/2)
+    m4_ = (y6_ - y2_)/(x6_ - x2_)
+    
+    # Triangle with clearance
+    u1, v1 = 460 - 5, 25 - 5
+    u2, v2 = 460 - 5, 225 + 5
+    u3, v3 = 510 + 10, 125
+    t1 = (v2 - v3)/(u2 - u3)
+    t2 = (v1 - v3)/(u1 - u3)
+    
+    # Triangle without clearance
+    u1_, v1_ = 460, 25 + 9.48
+    u2_, v2_ = 460, 225 - 9.48
+    u3_, v3_ = 510 , 125
+    t1_ = (v2_ - v3_)/(u2_ - u3_)
+    t2_ = (v1_ - v3_)/(u1_ - u3_)
+    canvas_ = canvas.copy()
+    for j in range(canvas.shape[0]):
+        for i in range(canvas.shape[1]):
+            # create rectangles with clearance.
+            if(i>=95 and i < 155 and j<=105):
+                canvas_[j,i] = [255, 0 ,0]
+            if(i>=95 and i < 155 and j>=145):
+                canvas_[j,i] = [255, 0 ,0]
+                
+            # create rectangles without clearance
+            if(i>=100 and i < 150 and j<=100):
+                canvas_[j,i] = [255, 255 ,255]
+            if(i>=100 and i < 150 and j>=150):
+                canvas_[j,i] = [255, 255 ,255]
+            
+            # create hexagon with clearance
+            # divide the hexagon into 2 halves and compute line equations for each
+            if(i>=(300 - h - 5) and i < 300 and ((j - m1*i - y1 + m1*x1)>= 0 and (j - m2*i - y3 + m2*x3)<= 0)):
+                canvas_[j, i] = [255, 0, 0]
+            if(i<(300 + h + 5) and i >= 300 and ((j - m3*i - y4 + m3*x4)<= 0 and (j - m4*i - y2 + m4*x2)>= 0)):
+                canvas_[j, i] = [255, 0, 0]
+                
+            # create hexagon without clearance
+            # divide the hexagon into 2 halves and compute line equations for each
+            if(i>=(300 - h) and i < 300 and ((j - m1_*i - y1_ + m1_*x1_)>= 0 and (j - m2_*i - y3_ + m2_*x3_)<= 0)):
+                canvas_[j, i] = [255, 255, 255]
+            if(i<(300 + h) and i >= 300 and ((j - m3_*i - y4_ + m3_*x4_)<= 0 and (j - m4_*i - y2_+ m4_*x2_>= 0))):
+                canvas_[j, i] = [255, 255, 255]
+                
+            # create triangle with clearance
+            if(i>=(460 - 5) and ((j - t1*i - v3 + t1*u3)<= 0 and (j - t2*i - v3 + t2*u3)>= 0)):
+                canvas_[j, i] = [255, 0, 0]
+            # create triangle without clearance
+            if(i>(460) and ((j - t1_*i - v3_ + t1_*u3_)<= 0 and (j - t2_*i - v3_ + t2_*u3_)>= 0)):
+                canvas_[j, i] = [255, 255, 255]
+            
+            # clearance for walls
+            if(i<5 or j<5 or i>594 or j>244):
+                canvas_[j, i] = [255, 0 ,0]
+            
+    return canvas_
